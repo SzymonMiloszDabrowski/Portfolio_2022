d94094ded01560b5f25704639a34de6475f16831
Szymon Dąbrowski <szymonmiloszdabrowski@gmail.com>
2022-10-26 12:54:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Portfolio
+
+This is my simple portfolio page with my projects and form to contact. I decided to create this as simple as possible, that is why i used only HTML, Less and Javascript. I belive that decision also provide on better SEO and UX.
+
+[Go to page](https://szymon.debno.com/)
---
+++ app.js
@@ -0,0 +1,55 @@
+const App = {
+	handleHamburger: function () {
+		const nav = document.querySelector('header nav');
+		const list = document.querySelector('header nav ul');
+
+		if (nav.classList.contains('toggled')) {
+			list.removeAttribute('style');
+		} else {
+			list.style.height = list.scrollHeight + 'px';
+		}
+		nav.classList.toggle('toggled');
+	},
+
+	listenEvents: function () {
+		document.querySelectorAll('.scroll_to_about').forEach((el) => {
+			el.addEventListener('click', () => {
+				document.querySelector('#about_section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+			});
+		});
+
+		document.querySelectorAll('.scroll_to_works').forEach((el) => {
+			el.addEventListener('click', () => {
+				document.querySelector('#works_section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+			});
+		});
+
+		document.querySelectorAll('.scroll_to_contact').forEach((el) => {
+			el.addEventListener('click', () => {
+				document.querySelector('#contact_section').scrollIntoView({ behavior: 'smooth', block: 'start' });
+			});
+		});
+
+		document.querySelector('.hamburger').addEventListener('click', (e) => {
+			this.handleHamburger(e);
+		});
+
+		document.querySelector('body').addEventListener('click', (e) => {
+			const nav = document.querySelector('header nav');
+			const list = document.querySelector('header nav ul');
+			const hamburger = document.querySelector('.hamburger');
+
+			if (nav.classList.contains('toggled') && !(hamburger == e.target) && !hamburger.contains(e.target) && !(list == e.target) && !list.contains(e.target)) {
+				this.handleHamburger(e);
+			}
+		});
+	},
+
+	init: function () {
+		this.listenEvents();
+	},
+};
+
+window.onload = () => {
+	App.init();
+};
